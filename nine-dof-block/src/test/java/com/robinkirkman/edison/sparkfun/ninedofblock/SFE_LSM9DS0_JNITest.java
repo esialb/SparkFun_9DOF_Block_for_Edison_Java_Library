52c164f00aa1d6d31173f23535f7590169bb5e22
robin <robin.kirkman@gmail.com>
2016-02-02 22:29:39 -0500

    Accel test that tries to read data

--- nine-dof-block/src/test/java/com/robinkirkman/edison/sparkfun/ninedofblock/SFE_LSM9DS0_JNITest.java
+++ nine-dof-block/src/test/java/com/robinkirkman/edison/sparkfun/ninedofblock/SFE_LSM9DS0_JNITest.java
@@ -1,10 +1,21 @@
 package com.robinkirkman.edison.sparkfun.ninedofblock;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
 
 public class SFE_LSM9DS0_JNITest {
 	@Test
-	public void testNew() {
-		new SFE_LSM9DS0_JNI();
+	public void testNew() throws Exception {
+		SFE_LSM9DS0_JNI dof = new SFE_LSM9DS0_JNI();
+		dof.begin();
+		long stop = System.currentTimeMillis() + TimeUnit.MILLISECONDS.convert(10, TimeUnit.SECONDS);
+		while(System.currentTimeMillis() < stop) {
+			dof.readAccel();
+			if(dof.newXData()) {
+				System.out.println(String.format("%f %f %f", dof.ax, dof.ay, dof.az));
+			} else
+				Thread.sleep(50);
+		}
 	}
 }
