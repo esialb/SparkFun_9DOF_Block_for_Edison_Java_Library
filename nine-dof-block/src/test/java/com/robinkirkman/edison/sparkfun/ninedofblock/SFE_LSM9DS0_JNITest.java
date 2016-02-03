6fad12a7f39d3a63062422d1154963c46785df81
robin <robin.kirkman@gmail.com>
2016-02-02 22:03:40 -0500

    Simple instantiation test

---
+++ nine-dof-block/src/test/java/com/robinkirkman/edison/sparkfun/ninedofblock/SFE_LSM9DS0_JNITest.java
@@ -0,0 +1,10 @@
+package com.robinkirkman.edison.sparkfun.ninedofblock;
+
+import org.junit.Test;
+
+public class SFE_LSM9DS0_JNITest {
+	@Test
+	public void testNew() {
+		new SFE_LSM9DS0_JNI();
+	}
+}
