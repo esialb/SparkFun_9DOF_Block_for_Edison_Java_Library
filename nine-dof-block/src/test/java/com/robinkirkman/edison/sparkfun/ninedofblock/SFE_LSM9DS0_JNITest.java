82422aca694706e16167935772de18371e171f20
robin <robin.kirkman@gmail.com>
2016-02-02 23:37:41 -0500

    faster sampling for shorter period

--- nine-dof-block/src/test/java/com/robinkirkman/edison/sparkfun/ninedofblock/SFE_LSM9DS0_JNITest.java
+++ nine-dof-block/src/test/java/com/robinkirkman/edison/sparkfun/ninedofblock/SFE_LSM9DS0_JNITest.java
@@ -9,13 +9,13 @@ public class SFE_LSM9DS0_JNITest {
 	public void testAccel() throws Exception {
 		SFE_LSM9DS0_JNI dof = new SFE_LSM9DS0_JNI();
 		dof.begin();
-		long stop = System.currentTimeMillis() + TimeUnit.MILLISECONDS.convert(10, TimeUnit.SECONDS);
+		long stop = System.currentTimeMillis() + TimeUnit.MILLISECONDS.convert(5, TimeUnit.SECONDS);
 		while(System.currentTimeMillis() < stop) {
 			if(dof.newXData()) {
 				dof.readAccel();
 				System.out.println(String.format("accel: %f %f %f", dof.ax, dof.ay, dof.az));
 			} else
-				Thread.sleep(50);
+				Thread.sleep(5);
 		}
 	}
 
@@ -23,13 +23,13 @@ public class SFE_LSM9DS0_JNITest {
 	public void testGyro() throws Exception {
 		SFE_LSM9DS0_JNI dof = new SFE_LSM9DS0_JNI();
 		dof.begin();
-		long stop = System.currentTimeMillis() + TimeUnit.MILLISECONDS.convert(10, TimeUnit.SECONDS);
+		long stop = System.currentTimeMillis() + TimeUnit.MILLISECONDS.convert(5, TimeUnit.SECONDS);
 		while(System.currentTimeMillis() < stop) {
 			if(dof.newGData()) {
 				dof.readGyro();
 				System.out.println(String.format("gyro: %f %f %f", dof.gx, dof.gy, dof.gz));
 			} else
-				Thread.sleep(50);
+				Thread.sleep(5);
 		}
 	}
 
@@ -37,13 +37,13 @@ public class SFE_LSM9DS0_JNITest {
 	public void testMag() throws Exception {
 		SFE_LSM9DS0_JNI dof = new SFE_LSM9DS0_JNI();
 		dof.begin();
-		long stop = System.currentTimeMillis() + TimeUnit.MILLISECONDS.convert(10, TimeUnit.SECONDS);
+		long stop = System.currentTimeMillis() + TimeUnit.MILLISECONDS.convert(5, TimeUnit.SECONDS);
 		while(System.currentTimeMillis() < stop) {
 			if(dof.newMData()) {
 				dof.readMag();
 				System.out.println(String.format("mag: %f %f %f", dof.mx, dof.my, dof.mz));
 			} else
-				Thread.sleep(50);
+				Thread.sleep(5);
 		}
 	}
 }
